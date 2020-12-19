9afef1a2f0a56b2c6ad211328c9041ef0565b21b
Maciej <maciej.rek.kontakt@gmail.com>
2020-12-19 12:32:50 +0100

    Warmup two

--- tasks/warmup.py
+++ tasks/warmup.py
@@ -8,3 +8,17 @@ def warmup_one(input_data: str):
         result[0] += sign.lower() if index % 2 == 0 else sign.upper()
         result[1] += sign.upper() if index % 2 == 0 else sign.lower()
     return result
+
+
+def warmup_two(input_data: str):
+    if not isinstance(input_data, str):
+        return "Wrong input data"
+    input_data = input_data.lower()
+    result = dict()
+    for letter in input_data:
+        if result.get(letter):
+            result[letter] += 1
+        else:
+            result[letter] = 1
+
+    return {key: val for key, val in result.items() if val > 1}
--- tests/test_warmup.py
+++ tests/test_warmup.py
@@ -1,19 +1,36 @@
 import unittest
-from tasks.warmup import warmup_one
+from tasks.warmup import warmup_one, warmup_two
 
 
 class TestWarmup(unittest.TestCase):
 
-    def test_wrong_input_data(self):
+    def test_warmup_one_wrong_input_data(self):
         """
         Provide wrong input parameters.
         We expect "Wrong input data" as a result
         """
         self.assertEqual('Wrong input data', warmup_one(13))
 
-    def test_valid_input_data(self):
+    def test_warmup_one_valid_input_data(self):
         """
         Provide valid input parameters "abcd".
         We expect ['aBcD','AbCd'] as a result
         """
         self.assertEqual(['aBcD', 'AbCd'], warmup_one('abcd'))
+
+    def test_warmup_two_wrong_input_data(self):
+        """
+        Provide wrong input parameters.
+        We expect "Wrong input data" as a result
+        """
+        self.assertEqual('Wrong input data', warmup_two(13))
+
+    def test_warmup_two_valid_input_data(self):
+        """
+        Provide valid input parameters.
+        We expect dict with letter:quantity pairs as a result
+        """
+        self.assertEqual({"a": 3, "b": 2}, warmup_two("aaabbc"))
+        self.assertEqual({"a": 3, "b": 2}, warmup_two("aAabBc"))
+        self.assertEqual({}, warmup_two("aBcDeF"))
+        self.assertEqual({"a": 4, "b": 2, "r": 3}, warmup_two("RabarbArka"))
